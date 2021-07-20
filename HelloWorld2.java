d61068aa224a3c10e9a3e3d9e54309bd5be7b7c6
YLTeng2653 <44768330+YLTeng2653@users.noreply.github.com>
2021-07-20 17:52:15 +0800

    Create HelloWorld2.java

---
+++ HelloWorld2.java
@@ -0,0 +1,11 @@
+package com.lcpan.m02;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// hi, this is comment
+		System.out.println("Hello World! Hello Java!");
+		System.out.println("22222222222222");
+	}
+
+}

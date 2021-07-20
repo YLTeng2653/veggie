b8e311965661da3bcfc989f78a540fc91e64a4a9
YLTeng2653 <44768330+YLTeng2653@users.noreply.github.com>
2021-07-20 16:27:59 +0800

    修改了 HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ package com.lcpan.m02;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World! Hello Java!");
 
 	}
 
